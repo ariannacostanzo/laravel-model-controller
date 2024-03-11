e85f2e17b2a8818e20ad5856d1ed21a651426f98
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-11 15:58:04 +0100

    implementing style

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -7,6 +7,7 @@
     <title>Movies</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css' integrity='sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==' crossorigin='anonymous'/>
     @vite('resources/js/app.js')
+    <style> body {display: none}</style>
 </head>
 <body>
     <div class="container my-4">
